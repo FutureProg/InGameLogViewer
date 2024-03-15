b784062032c25e551f4fbd97707427ba9192adeb
Nick Morrison <nickmorrison09@gmail.com>
2024-03-14 23:24:21 -0400

    Logs now updated in real time, adding the Player.log now

--- Systems/InGameLogUISystem.cs
+++ Systems/InGameLogUISystem.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Trejak.InGameLogViewer.Systems
 {
@@ -20,7 +21,10 @@ namespace Trejak.InGameLogViewer.Systems
         private ValueBinding<string> m_LogContent;
 
         private FileSystemWatcher logFolderWatcher;
-        private FileSystemWatcher fileContentWatcher;
+        private FileSystemWatcher outerFolderWatcher;
+        //private FileSystemWatcher fileContentWatcher;
+
+        const string PlayerLog = "Player.log";
 
 
         protected override void OnCreate()
@@ -28,17 +32,30 @@ namespace Trejak.InGameLogViewer.Systems
             base.OnCreate();
 
             logFolderWatcher = new FileSystemWatcher(LogManager.kDefaultLogPath, "*.log");
-            logFolderWatcher.NotifyFilter = NotifyFilters.FileName;
+            //logFolderWatcher.NotifyFilter = NotifyFilters.FileName;
             logFolderWatcher.Created += OnLogFileCreated;
             logFolderWatcher.Deleted += OnLogFileDeleted;
-            logFolderWatcher.Renamed += OnLogFileRenamed;               
+            logFolderWatcher.Renamed += OnLogFileRenamed;
+            logFolderWatcher.Changed += OnLogFileChanged;
+            logFolderWatcher.EnableRaisingEvents = true;
 
-            fileContentWatcher = new FileSystemWatcher(LogManager.kDefaultLogPath, "Player.log");
-            fileContentWatcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.Size;
-            fileContentWatcher.Changed += OnLogFileChanged;
+            outerFolderWatcher = new FileSystemWatcher(Application.persistentDataPath, "*.log");
+            outerFolderWatcher.Created += OnLogFileCreated;
+            outerFolderWatcher.Deleted += OnLogFileDeleted;
+            outerFolderWatcher.Renamed += OnLogFileRenamed;
+            outerFolderWatcher.Changed += OnLogFileChanged;
+            outerFolderWatcher.EnableRaisingEvents = true;
 
 
-            var filesDefault = new string[] { "abc", "def", "ghi", "jkl", "mno" };
+
+            //fileContentWatcher = new FileSystemWatcher(LogManager.kDefaultLogPath, "");
+            ////fileContentWatcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.Size | NotifyFilters.CreationTime | NotifyFilters.Attributes;
+            //fileContentWatcher.Changed += OnLogFileChanged;
+            //fileContentWatcher.Created += OnLogFileCreated;
+            //fileContentWatcher.Deleted += OnLogFileDeleted;
+            //fileContentWatcher.Renamed += OnLogFileRenamed;
+            //fileContentWatcher.EnableRaisingEvents = true;
+
             AddBinding(m_LogPanelEnabled = new ValueBinding<bool>("Trejak.InGameLogViewer", "LogPanelEnabled", true));
             AddBinding(m_LogFiles = new ValueBinding<string[]>("Trejak.InGameLogViewer", "LogFiles", GetLogFileNames(), new ArrayWriter<string>()));
             AddBinding(m_LogFileSelected = new ValueBinding<string>("Trejak.InGameLogViewer", "LogFileSelected", ""));
@@ -47,6 +64,7 @@ namespace Trejak.InGameLogViewer.Systems
             AddBinding(new TriggerBinding("Trejak.InGameLogViewer", "LogPanelClosed", LogPanelClosed));
             AddBinding(new TriggerBinding("Trejak.InGameLogViewer", "LogPanelToggled", LogPanelToggled));
             AddBinding(new TriggerBinding<string>("Trejak.InGameLogViewer", "OpenLogFile", OpenLogFile));
+            Mod.log.Info("InGameLogUISystem created.");
             
         }
 
@@ -63,7 +81,12 @@ namespace Trejak.InGameLogViewer.Systems
 
         private string GetLogFileContent(string fileName)
         {
-            string re = File.ReadAllText(Path.Combine(LogManager.kDefaultLogPath, fileName));
+            var path = Path.Combine(LogManager.kDefaultLogPath, fileName);
+            if (fileName == PlayerLog)
+            {
+                path = Path.Combine(Application.persistentDataPath, fileName);
+            }
+            string re = File.ReadAllText(path);
             return re;
         }
 
@@ -74,20 +97,24 @@ namespace Trejak.InGameLogViewer.Systems
 
         private void OnLogFileCreated(object sender, FileSystemEventArgs evt)
         {
+            //Mod.log.Info($"File {evt.Name} created: {evt.ChangeType}");
             UpdateLogFileList();
         }
 
         private void OnLogFileDeleted(object sender, FileSystemEventArgs evt)
-        {            
+        {
+            //Mod.log.Info($"File {evt.Name} deleted: {evt.ChangeType}");
+            UpdateLogFileList();
             if (evt.Name == m_LogFileSelected.value)
             {
-                OpenLogFile("Player.log");
-            }
-            UpdateLogFileList();
+                if (m_LogFiles.value.Length > 0)
+                    OpenLogFile(m_LogFiles.value[0]);
+            }            
         }
 
         private void OnLogFileRenamed(object sender, RenamedEventArgs evt)
         {
+            //Mod.log.Info($"File {evt.OldName}, moved to {evt.Name} with event {evt.ChangeType}");
             UpdateLogFileList();
             if (evt.OldName == m_LogFileSelected.value)
             {                
@@ -98,14 +125,17 @@ namespace Trejak.InGameLogViewer.Systems
         private void OnLogFileChanged(object sender, FileSystemEventArgs evt)
         {
             // Only for the fileContentWatcher
+            //Mod.log.Info($"File {evt.Name} changed with event {evt.ChangeType}");
             if (evt.ChangeType != WatcherChangeTypes.Changed)
             {
                 return;
             }
+            //Mod.log.Info($"File changed {evt.Name} vs {m_LogFileSelected.value}");
             if (evt.Name == m_LogFileSelected.value)
             {
+                //Mod.log.Info("File changed");
                 UpdateLogFileContent(m_LogFileSelected.value);
-            }            
+            }
         }
 
         private void UpdateLogFileContent(string logFile)
@@ -117,7 +147,7 @@ namespace Trejak.InGameLogViewer.Systems
         private void OpenLogFile(string logFile)
         {            
             m_LogFileSelected.Update(logFile);
-            fileContentWatcher.Filter = logFile;
+            //fileContentWatcher.Filter = logFile;            
             UpdateLogFileContent(logFile);
         }
 
@@ -131,11 +161,28 @@ namespace Trejak.InGameLogViewer.Systems
             m_LogPanelEnabled.Update(!m_LogPanelEnabled.value);
         }
 
+        protected override void OnStopRunning()
+        {
+            base.OnStopRunning();
+            logFolderWatcher.EnableRaisingEvents = false;
+            outerFolderWatcher.EnableRaisingEvents = false;
+            //fileContentWatcher.EnableRaisingEvents = false;
+        }
+
+        protected override void OnStartRunning()
+        {
+            base.OnStopRunning();
+            logFolderWatcher.EnableRaisingEvents = true;
+            outerFolderWatcher.EnableRaisingEvents = true;
+            //fileContentWatcher.EnableRaisingEvents = true;
+        }
+
         protected override void OnDestroy()
         {
             base.OnDestroy();
             logFolderWatcher.Dispose();
-            fileContentWatcher.Dispose();
+            outerFolderWatcher.Dispose();
+            //fileContentWatcher.Dispose();
         }
 
     }
