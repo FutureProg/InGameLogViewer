4f9f56977bd6d95a5ceed954c1be79259b800957
Nick Morrison <nickmorrison09@gmail.com>
2024-03-27 00:58:37 -0400

    Progress

--- InGameLogViewer-UI/src/index.tsx
+++ InGameLogViewer-UI/src/index.tsx
@@ -1,6 +1,7 @@
-import { ModRegistrar } from "cs2/modding";
+import { ModRegistrar, ModuleRegistryAppend } from "cs2/modding";
+import { useRem } from "cs2/utils";
 import { LogPanel } from "mods/LogPanel";
-import { PanelActivationButton } from "mods/PanelActivationButton";
+import { PanelActivationButton, createPanelActivationButton } from "mods/PanelActivationButton";
 
 const register: ModRegistrar = (moduleRegistry) => {
     // While launching game in UI development mode (include --uiDeveloperMode in the launch options)
@@ -9,7 +10,13 @@ const register: ModRegistrar = (moduleRegistry) => {
     // - use the useModding() hook to access exposed UI, api and native coherent engine interfaces. 
     // Good luck and have fun!
     moduleRegistry.append('Menu', LogPanel, 1);
-    moduleRegistry.append('Menu', PanelActivationButton, 2);
+    moduleRegistry.append('Menu', createPanelActivationButton("menu"), 2);
+
+    moduleRegistry.append('Game', LogPanel, 1);    
+    moduleRegistry.append('GameBottomRight', createPanelActivationButton("flat"));
+
+    moduleRegistry.append('Editor', LogPanel, 1);    
+    moduleRegistry.append('Editor', createPanelActivationButton("flat", {x: 20, y: 20}));
 }
 
 export default register;
--- InGameLogViewer-UI/src/mods/PanelActivationButton.tsx
+++ InGameLogViewer-UI/src/mods/PanelActivationButton.tsx
@@ -1,8 +1,23 @@
-import { Button } from "cs2/ui"
+import { Button, ButtonProps, Portal } from "cs2/ui"
 import { toggleLogPanel } from "./bindings";
+import { Number2 } from "cs2/bindings";
 
-export const PanelActivationButton = () => {
+export const PanelActivationButton = (buttonVariant: "menu" | "flat", position?: Number2) => { 
     return (
-        <Button variant="menu" onSelect={toggleLogPanel} onMouseOver={() => console.log("Hello UI")}>Logs</Button>
+        <Button variant={buttonVariant} onSelect={toggleLogPanel} onMouseOver={() => console.log("Hello UI")}>Logs</Button>
     );
 }
+
+export const createPanelActivationButton = (buttonVariant: "menu" | "flat", position?: Number2) => () => (
+    <Portal>
+        <Button 
+            variant="menu" 
+            onSelect={toggleLogPanel} 
+            onClick={toggleLogPanel}
+            onMouseOver={() => console.log("Hello UI")}
+            style={position? {'position': "absolute", top: `${position.y} rem`, left: `${position.x} rem`} : {}}
+            >
+            Logs
+        </Button>
+    </Portal>    
+)
